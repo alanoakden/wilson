1746584b8798e0092af74e7b0d564f629f997275
Alan Oakden <alan.oakden@weareaccess.co.uk>
2021-01-29 00:43:48 +0000

    Add Wilson storybook theme, and update config.js to use main.js and preview.js ahead of storybook 7.

---
+++ themes/wilson_theme_starterkit/.storybook/preview.js
@@ -0,0 +1,4 @@
+import Twig from 'twig';
+import twigDrupal from 'twig-drupal-filters';
+// Add the filters to Twig instance.
+twigDrupal(Twig);
---
+++ themes/wilson_theme_starterkit/.storybook/wilsonStorybookTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Wilson Storybook theme',
+  brandUrl: 'https://www.weareaccess.co.uk/',
+  brandImage: 'https://raw.githubusercontent.com/accessdigital/wilson/master/themes/wilson_theme_starterkit/logo.svg'
+});
