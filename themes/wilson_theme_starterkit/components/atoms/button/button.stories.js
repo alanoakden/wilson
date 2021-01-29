8919bef2093fbcc695db30f8fc5dfdabb549293b
Alan Oakden <alan.oakden@weareaccess.co.uk>
2021-01-29 00:17:23 +0000

    Add button components to storybook and in turn test them on a theme template override.

---
+++ themes/wilson_theme_starterkit/components/atoms/button/button.stories.js
@@ -0,0 +1,43 @@
+export default { title: 'Buttons' };
+
+import block from './button.twig';
+import '../../../css/styles.css'
+import drupalAttribute from 'drupal-attribute'
+
+export const default_button = () => (
+    block({
+      attributes: new drupalAttribute(),
+      type: 'primary',
+      button_label: "I'm a block!",
+      items: [
+        {
+          content: {
+            '#url': {
+              isExternal: 'isxternal'
+            },
+            '#title': 'Button Title'
+          }
+        },
+      ],
+
+    })
+);
+
+export const secondary_button = () => (
+    block({
+      attributes: new drupalAttribute(),
+      button_label: "I'm a block!",
+      type: 'secondary',
+      items: [
+        {
+          content: {
+            '#url': {
+              isExternal: 'isxternal'
+            },
+            '#title': 'Button Title'
+          }
+        },
+      ],
+
+    })
+);
