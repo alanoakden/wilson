222e3dcbc7a693a6ae3b25c91577423844a26d2b
Alan Oakden <alan.oakden@weareaccess.co.uk>
2021-01-31 11:30:39 +0000

    Dynamic library hook, button css and js added. Storybook and Drupal working. Styling is broken due to how Drupal is aggregating it.

--- themes/wilson_theme_starterkit/.storybook/webpack.config.js
+++ themes/wilson_theme_starterkit/.storybook/webpack.config.js
@@ -11,5 +11,11 @@ module.exports = ({ config }) => {
       },
     ],
   });
+
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
   return config;
 };
--- themes/wilson_theme_starterkit/components/atoms/button/button.stories.js
+++ themes/wilson_theme_starterkit/components/atoms/button/button.stories.js
@@ -1,6 +1,7 @@
 export default { title: 'Buttons' };
 
 import button from './button.twig';
+import './button.css';
 import '../../../css/styles.css'
 import drupalAttribute from 'drupal-attribute'
 
